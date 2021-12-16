f7714b5cff043684966be8fd81db95492b5b5915
emrecoskun705 <emrecoskun705@gmail.com>
2021-12-17 00:10:44 +0300

    add login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,14 +5,30 @@ import 'package:addiction_app/utils/size_config.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_radio_group/flutter_radio_group.dart';
 import 'dart:async';
 
-class LoginScreen extends StatelessWidget {
+import 'package:gender_picker/gender_picker.dart';
+import 'package:gender_picker/source/enums.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController classText = TextEditingController();
+
   TextEditingController age = TextEditingController();
+
   TextEditingController gender = TextEditingController();
+
   TextEditingController parents = TextEditingController();
 
+  final _listHorizontal = ["Sağ", "Hayatta değil"];
+  final _parentRelation = ['Birlikte yaşıyor', 'Ayrı'];
+  int? _parentIndex = null;
+  int? _indexHorizontal = null;
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -23,66 +39,115 @@ class LoginScreen extends StatelessWidget {
       return userInfo.add({'deneme': 'denem1', 'deneme2': 'deneme2'});
     }
 
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Container();
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Scaffold(
-            body: SingleChildScrollView(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [
-                        Color(0xFFC6FFDD),
-                        Color(0xFFFBD786),
-                        Color(0xFFF7797d),
-                      ]),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color(0xFFC6FFDD),
+                  Color(0xFFFBD786),
+                  Color(0xFFF7797d),
+                ]),
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InfoBubbleWidget(
+                    message:
+                        'Lütfen anketin daha verimli olması için bilgilerinizi girer misiniz'),
+                InfoTextField(
+                    hint: 'Yaşınızı giriniz',
+                    prefixIcon: Icons.eight_k,
+                    controller: classText),
+                InfoTextField(
+                    hint: 'Sınıfınızı giriniz',
+                    prefixIcon: Icons.eight_k,
+                    controller: age),
+                GenderPickerWithImage(
+                  onChanged: (Gender? gender) {},
+                  maleText: 'Erkek',
+                  femaleText: 'Kadın',
+                  selectedGender: null,
                 ),
-                child: SafeArea(
-                  child: Column(
-                    children: [
-                      InfoBubbleWidget(
-                          message:
-                              'Lütfen anketin daha verimli olması için bilgilerinizi girer misiniz'),
-                      InfoTextField(
-                          hint: 'Yaşınızı giriniz',
-                          prefixIcon: Icons.eight_k,
-                          controller: classText),
-                      InfoTextField(
-                          hint: 'Sınıfınızı giriniz',
-                          prefixIcon: Icons.eight_k,
-                          controller: age),
-                      InfoTextField(
-                          hint: 'Sınıfınızı giriniz',
-                          prefixIcon: Icons.eight_k,
-                          controller: gender),
-                      InfoTextField(
-                          hint: 'Sınıfınızı giriniz',
-                          prefixIcon: Icons.eight_k,
-                          controller: parents),
-                      RoundedButton(
-                          title: 'click',
-                          bgColor: Colors.lightBlue,
-                          onPressed: () {
-                            addUser();
-                          })
-                    ],
-                  ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: getProportionateScreenHeight(20)),
+                  child: FlutterRadioGroup(
+                      titles: _listHorizontal,
+                      labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: getProportionateScreenHeight(15),
+                          fontWeight: FontWeight.bold),
+                      labelVisible: true,
+                      label: "Anne",
+                      activeColor: Colors.blue,
+                      titleStyle: TextStyle(fontSize: 14),
+                      defaultSelected: _indexHorizontal,
+                      orientation: RGOrientation.HORIZONTAL,
+                      onChanged: (index) {
+                        setState(() {
+                          _indexHorizontal = index;
+                        });
+                      }),
                 ),
-              ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: getProportionateScreenHeight(20)),
+                  child: FlutterRadioGroup(
+                      titles: _listHorizontal,
+                      labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: getProportionateScreenHeight(15),
+                          fontWeight: FontWeight.bold),
+                      labelVisible: true,
+                      label: "Baba",
+                      activeColor: Colors.blue,
+                      titleStyle: TextStyle(fontSize: 14),
+                      defaultSelected: _indexHorizontal,
+                      orientation: RGOrientation.HORIZONTAL,
+                      onChanged: (index) {
+                        setState(() {
+                          _indexHorizontal = index;
+                        });
+                      }),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: getProportionateScreenHeight(20)),
+                  child: FlutterRadioGroup(
+                      titles: _parentRelation,
+                      labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: getProportionateScreenHeight(15),
+                          fontWeight: FontWeight.bold),
+                      labelVisible: true,
+                      label: "Aile Durumu",
+                      activeColor: Colors.blue,
+                      titleStyle: TextStyle(fontSize: 14),
+                      defaultSelected: _parentIndex,
+                      orientation: RGOrientation.HORIZONTAL,
+                      onChanged: (index) {
+                        setState(() {
+                          _parentIndex = index;
+                        });
+                      }),
+                ),
+                RoundedButton(
+                    title: 'click',
+                    bgColor: Colors.lightBlue,
+                    onPressed: () {
+                      addUser();
+                    })
+              ],
             ),
-          );
-        }
-
-        return Container();
-      },
+          ),
+        ),
+      ),
     );
   }
 }
